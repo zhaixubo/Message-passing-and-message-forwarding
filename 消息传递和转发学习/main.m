a3d6d1e5c9df00d2907f3b46260f869528a1b61d
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-04-12 12:15:30 +0800

    Initial Commit

---
+++ 消息传递和转发学习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  消息传递和转发学习
+//
+//  Created by 翟旭博 on 2023/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
